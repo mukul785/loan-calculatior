4b4a5abb1d998f9ffcd107f1ca3d8a9af77e7228
mukul785 <156766802+mukul785@users.noreply.github.com>
2024-06-18 12:14:51 +0530

    c

--- src/index.js
+++ src/index.js
@@ -4,13 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// ReactDOM.render(
-//   <Router>
-//     <App />
-//   </Router>,
-//   document.getElementById('root')
-// );
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
